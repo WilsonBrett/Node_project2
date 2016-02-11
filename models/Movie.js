60bf13bbc3910da3c5cd420dd4d4978629873eb4
Brett Wilson <brettwilson7@yahoo.com>
2016-02-10 18:11:29 -0600

    modifed movies routes

---
+++ models/Movie.js
@@ -0,0 +1,19 @@
+/*
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+
+     email: {type: String, required: true},
+	 password: {type: Number, required: true},
+     location: {type: String, required: true},
+     favs: {type: Array}
+
+});
+
+var Movie = mongoose.model('User', userSchema);
+
+
+
+// Make this available to our other files
+module.exports = User;
+*/
--- routes/movies.js
+++ routes/movies.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
 
+//5)
 router.get('/movies', function(req, res, next) {
   
   res.render('movies');
+});
+
+//6)
+router.get('/movies/:id', function(req, res, next) {
+	var id = req.params.id;
+
+	res.render('show_movie');
 });
 
 module.exports = router;
