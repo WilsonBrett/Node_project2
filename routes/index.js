cd3be2e56891f94a0dff66717bd813639ad320fb
Brett Wilson <brettwilson7@yahoo.com>
2016-02-11 23:57:02 -0600

    favicon

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,34 @@ router.get('/', function(req, res, next) {
   res.render('index', {msg: null});
 });
 
+//2
+router.post('/', function(req, res, next) {
+	var login_email = req.body.email;
+	var login_password = req.body.password;
+
+	if(login_email === null || login_email === "" || login_email === " ") {
+		alert('please enter a valid email');
+	} else if(login_password === null || login_password === "" || login_password === " ") {
+		alert('please enter a valid password');
+	} else {
+		User.findOne({'email':login_email}, 'email password', function(err, result) {
+			if (err) {
+				console.log(err);
+				throw err;
+			}
+			
+			if (result === null) {
+				res.render('index', { msg : 'Email not found.  Please register or try again.' });
+			} else {//email found - check password.
+				if (result.password === login_password) {
+					res.render('movies', {email: login_email});
+				} else {
+					res.render('index', { msg : 'Password is incorrect.  Please try again.' });
+				}
+			}	
+		}); 
+	}
+});
+
+
 module.exports = router;
