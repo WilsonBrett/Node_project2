3ca71a9857a6c99d4786161c50caeb1d0feefa67
Brett Wilson <brettwilson7@yahoo.com>
2016-02-15 10:28:27 -0600

    fixing changes from alter routes attempt

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,21 @@ var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   //console.log(req.session);
   //console.log('--------------------');
   //console.log(req.cookies); //cookie parser should populate this from the headers
-  
-  //res.send('some text for now.');
-  res.render('index', {msg: null, val: null});
+  res.render('index', {msg: null});
 });
 
 //2
-router.post('/', function(req, res, next) {
+router.post('/index', function(req, res, next) {
 	var login_email = req.body.email;
 	var login_password = req.body.password;
 
 	if(login_email === null || login_email === "" || login_email === " ") {
-		//console.log('Houstin we have an empty email');
 		res.send('Houstin we have an empty email');
 	} else if(login_password === null || login_password === "" || login_password === " ") {
-		//console.log('Houstin we have an empty password');
 		res.send('Houstin we have an empty password');
 	} else {
 		User.findOne({'email':login_email}, 'email password', function(err, result) {
@@ -32,7 +29,7 @@ router.post('/', function(req, res, next) {
 				//research err.code of 11000
 			}
 			if (!result) {
-				res.render('index', { msg : 'Email or password is incorrect.  Please register or try again.', val: null});
+				res.render('index', { msg : 'Email or password is incorrect.  Please register or try again.'});
 			} else {//email found - check password.
 				if (result.password === login_password) {//successful login
 					console.log('------------------');
@@ -40,7 +37,7 @@ router.post('/', function(req, res, next) {
 					console.log(req.session);
 					res.redirect('/movies');
 				} else {
-					res.render('index', { msg : 'Email or password is incorrect.  Please register or try again.', val: login_email });
+					res.render('index', { msg : 'Email or password is incorrect.  Please register or try again.'});
 				}
 			}	
 		}); 
