1fc976c6ff3c31932efe25b56ccd0e8989d3ca42
Brett Wilson <brettwilson7@yahoo.com>
2016-02-16 09:16:42 -0600

    movies details controller logic

--- routes/login.js
+++ routes/login.js
@@ -5,10 +5,6 @@ var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-  console.log(req.session);
-  //console.log('--------------------');
-  //console.log(req.cookies); //cookie parser should populate this from the headers
   res.render('login', {msg: null});
 });
 
